ccb4cbe4acaecf921a3955b55fc87192e51c15a8
harsh <sharmaharshu139@gmail.com>
2025-11-30 13:21:14 +0530

    images added

--- Validation.py
+++ Validation.py
@@ -3,7 +3,9 @@ import pickle
 import re
 from scipy.sparse import csr_matrix, hstack
 from sklearn.metrics import accuracy_score, classification_report
-
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 # -------------- CLEAN FUNCTION --------------
 def clean_text(t):
     t = str(t).lower()
@@ -59,3 +61,12 @@ y_pred = model.predict(X_val)
 # -------------- EVALUATE --------------
 print("\nValidation Accuracy:", accuracy_score(y_true, y_pred))
 print("\nClassification Report:\n", classification_report(y_true, y_pred))
+
+cm = confusion_matrix(y_true, y_pred)
+plt.figure(figsize=(6,4))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("Confusion Matrix")
+plt.savefig("confusion_matrix.png")
+print("Saved confusion_matrix.png")
